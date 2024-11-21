09de1121e5918e436dfda081e850111ffb873e13
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2024-11-21 16:34:54 -0300

    implementado exemplo de insercao de dados

--- jdbc_sql/src/application/Main.java
+++ jdbc_sql/src/application/Main.java
@@ -10,6 +10,7 @@ import java.sql.Statement;
 public class Main {
     public static void main(String[] args) {
 
+        //select data example
         Connection conn = null;
         Statement st = null;
         ResultSet rs = null;
---
+++ jdbc_sql/src/insertData_example/Main.java
@@ -0,0 +1,51 @@
+package insertData_example;
+
+import db.DB;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+
+            conn = DB.getConnection();
+            st = conn.prepareStatement("INSERT INTO seller " +
+                    "(Name, Email, BirthDate, BaseSalary, DepartmentId) " +
+                    "VALUES " +
+                    "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+            st.setString(1, "Carl Blue");
+            st.setString(2, "carl@gmail.com");
+            st.setDate(3, new Date(sdf.parse("22/04/1985").getTime()));
+            st.setDouble(4, 2500.0);
+            st.setInt(5, 3);
+
+            int affectedRows = st.executeUpdate();
+
+            if (affectedRows > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while(rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id);
+                }
+            } else {
+                System.out.println("No rows affected");
+            }
+        }
+        catch (SQLException | ParseException e) {
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
